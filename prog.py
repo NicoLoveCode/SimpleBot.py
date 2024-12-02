1eb5f87cbd5dd2c520f4d51e5988cc8fd9306661
Nico <103645370+NicoLoveCode@users.noreply.github.com>
2024-12-02 05:12:25 +0000

    .

--- prog.py
+++ prog.py
@@ -5,15 +5,13 @@ import time
 # start? 
 start = input("Would you like to start?\n")
 
-if start == "yes" or start == "Yes":
+if start.lower() == "yes":
     time.sleep(0.5)
-    print("\nAlright, lets start the game...")
-elif start == "no" or start == "No":
+    print("\nAlright, let's start the game...")
+elif start.lower() == "no":
     time.sleep(0.5)
-    print("\nif you don't wanna play just dont click play 🙄🤦‍♂️")
-    quit(
-
-    )
+    print("\nIf you don't want to play, just don't click play 🙄🤦‍♂️")
+    quit()
 
 # where will you start your adventure?
 camp_options = ["Beach", "Forest", "Rainforest", "Tundra", "Desert"]
@@ -22,3 +20,26 @@ time.sleep(0.3)
 print("You will be starting in the " + camp + "!")
 
 # starting out
+time.sleep(0.3)
+start_message = "Welcome to the " + camp
+print(start_message)
+time.sleep(0.2)
+
+# Possible inventory items
+pos_beach_inv1 = ["Shells", "Rocks", "Sticks", "Fish"]
+pos_beach_inv2 = ["Fish", "Turtle Shell", "Clams", "Bow", "Arrows"]
+inv = ran.choice(pos_beach_inv1 + pos_beach_inv2)
+
+time.sleep(1)
+print("You looked around for a while and gathered some materials.")
+time.sleep(0.2)
+print("...")
+time.sleep(0.5)
+
+# Check inventory
+if inv in pos_beach_inv1:
+    print("You found some normal things, and also a washed-up fish. This is your current inventory: " + inv)
+elif inv in pos_beach_inv2:
+    print("You got lucky and found an old box with a bow and arrow! You used the arrows to get fish and collected some more food and items. This is your current inventory: " + inv)
+else:
+    print("This part has not yet been finished developing")
